36d6b09050e373456f0ccffb367a5ac2100c9ee1
Kbouti <121819340+Kbouti@users.noreply.github.com>
2023-03-23 11:20:12 -0700

    Complete exeercise 5 sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
-
-};
+const sumAll = function(first, second) {
+    if (typeof(first) == `number` && typeof(second) == `number` && ((first * second) > 0)) {
+      if (first < second){
+          let sum = 0;
+          for(i = first; i <= second; i++){
+              sum += i;
+          }
+          return sum;
+      }
+      else if (first > second){
+          let sum = 0;
+          for(i = second; i <= first; i++){
+              sum += i;
+          }
+          return sum;
+      }
+    } else {
+              return `ERROR`
+          }
+  }
 
 // Do not edit below this line
 module.exports = sumAll;
