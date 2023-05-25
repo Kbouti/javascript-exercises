3310c6b1c086c6a8a6e056d4030e4faa982d194c
Kbouti <121819340+Kbouti@users.noreply.github.com>
2023-05-25 12:17:24 -0700

    Completed fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    num = parseInt(num);
+    if (num <= 0){
+        console.log(`OOPS!`)
+        return `OOPS`
+    }
+    let array = [1, 1]
+    for (let i = 0; i <num; i++){
+        let newNum = array[i] + array[i+1]
+        array.push(newNum)
+    }
+    console.log(`final sequence: ${array}`);
+    answer = array[num-1];
+    console.log(`index at ${num}: ${answer}`)
+    return answer
 };
 
 // Do not edit below this line
