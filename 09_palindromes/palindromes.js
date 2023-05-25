03f9408a2aec9caee6bc9c9553713235117b213d
Kbouti <121819340+Kbouti@users.noreply.github.com>
2023-05-25 11:15:58 -0700

    Complete palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
-
-};
+const palindromes = function (string) {
+    let lowered = string.toLowerCase();
+   let firstArray = lowered.split(``);
+   for (let i = 0; i <=2; i++){
+    for (let i = 0; i <= firstArray.length; i++){
+        if ((firstArray[i] === ` `) || (firstArray[i] === `.`)|| (firstArray[i] === `,`)|| (firstArray[i] === `'`)|| (firstArray[i] === `?`)|| (firstArray[i] === `!`)){
+        firstArray.splice(i, 1)
+        }
+    }
+   }
+   let reverseArray = firstArray.slice().reverse();
+   let newString = firstArray.join(``);
+   let reverseString = reverseArray.join(``);
+return newString === reverseString;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
