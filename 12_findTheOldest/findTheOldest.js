4445a859ef97820275db01c7424a661ce0409e33
Kbouti <121819340+Kbouti@users.noreply.github.com>
2023-05-26 10:09:41 -0700

    Complete find the oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,25 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
 
+    for(let person of array){
+        if (person.yearOfDeath == null){
+            person.yearOfDeath = 2023;
+        }
+    }
+
+    array.sort(function(a, b){
+        if ((a.yearOfDeath - a.yearOfBirth) < (b.yearOfDeath - b.yearOfBirth)) {
+            return -1;
+        }
+        else if ((a.yearOfDeath - a.yearOfBirth) > (b.yearOfDeath - b.yearOfBirth)) {
+            return +1;
+        }
+        else if ((a.yearOfDeath - a.yearOfBirth) == (b.yearOfDeath - b.yearOfBirth)) {
+            return 0;
+        }
+      })
+      console.table(array);
+    console.log(array[array.length-1].name)
+     return (array[array.length-1])
 };
 
 // Do not edit below this line
